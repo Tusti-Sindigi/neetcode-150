fba334f835aacacaf36e64cac18bef40cd798c48
Tusti Sindigi <tustiisindigi@gmail.com>
2026-06-02 19:00:07 +0530

    group anagrams sol 2 -  theoretically better but not in reality

--- arrays-and-hasing/group-anagrams.cpp
+++ arrays-and-hasing/group-anagrams.cpp
@@ -1,28 +1,29 @@
 #include<vector>
 #include<string>
 #include<unordered_map>
-#include<algorithm>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> so;
-        unordered_map<string, int> map;
-        int k=0;
+        unordered_map<string, vector<string>> map;
 
         for(string st: strs)
         {
-            string str=st;
-            sort(str.begin(), str.end());
-            if(map.find(str)==map.end())
-            {
-                map.insert({str, k});
-                so.push_back({});
-                k++;
-            }
-            so[map.find(str)->second].push_back(st);
+            vector <int> k(26, 0);
+            for(char c: st)
+                k[c-'a']++;
+
+            string s=to_string(k[0]);
+            for(int i=1;i<26;i++)
+                s+=','+to_string(k[i]);
+
+            map[s].push_back(st);
         }
+
+        for(auto st: map)
+            so.push_back(st.second);
 
         return so;
     }
