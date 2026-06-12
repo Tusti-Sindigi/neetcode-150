ab47c227fa5808f3e4d201686391e1bdbadd254a
Tusti Sindigi <tustiisindigi@gmail.com>
2026-06-12 23:11:04 +0530

    Container with most water sol 1

---
+++ two-pointers/container-with-most-water.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) 
+    {
+        int l=0, r=height.size()-1;
+        int max_area=0;
+
+        while(l<r)
+        {
+            int area=min(height[l],height[r])*(r-l);
+
+            if(area>max_area)
+                max_area=area;
+
+            if(height[l]<=height[r])
+                l++;
+            else
+                r--;
+        }
+        return max_area;
+    }
+};
