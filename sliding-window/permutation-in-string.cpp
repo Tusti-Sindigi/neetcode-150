d1c7b55d954d61c7b146d7f568206a90bc10f431
Tusti Sindigi <tustiisindigi@gmail.com>
2026-06-21 23:51:42 +0530

    permutation in string sol 2

--- sliding-window/permutation-in-string.cpp
+++ sliding-window/permutation-in-string.cpp
@@ -1,41 +1,39 @@
 #include <string>
-#include<unordered_map>
+#include<vector>
 using namespace std;
-
-//v bad executuion time
 
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) 
     {
-        unordered_map<char, int> m2;
+        int n1=s1.size(), n2=s2.size();
 
-        for(char c:s1)
-            m2[c]++;
+        if(n1>n2)
+            return false;
 
-        unordered_map<char, int> m=m2;
+        vector<int> v1(26, 0);
+        vector<int> v2(26, 0);
 
         int i=0;
-        while(i<s2.length())
+
+        while(i<n1)
         {
-            int k=i+1;
+            v1[s1[i]-'a']++;
+            v2[s2[i]-'a']++;
+            i++;
+        }
+        
+        if(v1==v2)
+            return true;
 
-            while(i<s2.length() && m2.find(s2[i])!=m2.end())
-            {
-                m2[s2[i]]--;
+        while(i<n2)
+        {
+            v2[s2[i]-'a']++;
+            v2[s2[i-n1]-'a']--;
+            i++;
 
-                if(m2[s2[i]]==0)
-                    m2.erase(s2[i]);
-
-                i++;
-            }
-            if(m2.empty())
+            if(v1==v2)
                 return true;
-            else
-            {
-                i=k;
-                m2=m;
-            }   
         }
         return false;
     }
